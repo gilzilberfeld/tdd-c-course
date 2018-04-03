f8c84ad9357c69623e3bbc8ff0aa636152c1ee32
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-04-03 10:33:38 +0300

    Added suite support to LED Status

---
+++ src/Tests/leddriver_tests/LEDDriverTestSuite.h
@@ -0,0 +1,23 @@
+/*
+ * LEDDriverTestSuite.h
+ *
+ *  Created on: 3 ????? 2018
+ *      Author: Gil
+ */
+
+#ifndef TESTS_LEDDRIVER_TESTS_LEDDRIVERTESTSUITE_H_
+#define TESTS_LEDDRIVER_TESTS_LEDDRIVERTESTSUITE_H_
+
+class LEDDriverTestSuite {
+public:
+	LEDDriverTestSuite() {
+		// Setup
+	}
+	~LEDDriverTestSuite() {
+		// Teardown
+	}
+
+};
+
+
+#endif /* TESTS_LEDDRIVER_TESTS_LEDDRIVERTESTSUITE_H_ */
--- src/Tests/leddriver_tests/LEDDriverTests.cpp
+++ src/Tests/leddriver_tests/LEDDriverTests.cpp
@@ -1,15 +1,17 @@
 #include "../../frameworks/yaffut.h"
+#include "LEDDriverTestSuite.h"
+
 extern "C" void driverInitializeLEDs();
 extern "C" void driverTurnAllOn();
 extern "C" bool driverAreAllLEDsOn();
 
-TEST(All_LEDs_Are_Off_After_Initialization)
+SUITE_TEST(LEDDriverTestSuite, All_LEDs_Are_Off_After_Initialization)
 {
 	CHECK(!driverAreAllLEDsOn());
 }
 
 
-TEST(All_LEDs_Are_On_After_Turning_All_On)
+SUITE_TEST(LEDDriverTestSuite,All_LEDs_Are_On_After_Turning_All_On)
 {
 }
 
